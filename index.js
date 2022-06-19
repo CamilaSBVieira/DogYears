02aa461c19780267bf84d21aa47e90d68673e30a
CamilaSBVieira <camilag2.c@gmail.com>
2022-06-18 23:28:03 -0300

    Dog Years Project

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const botao = document.getElementById('botao');
+const age = document.getElementById('age');
+
+function convertToDogYears() {
+    let earlyYears;
+    let laterYears;
+    let myAgeInDogYears;
+    const humanAge = age.value;
+    const result = document.getElementById('result');
+    if (humanAge >= 2) {
+        earlyYears = 2;
+        myAgeInDogYears = earlyYears * 10.5;
+        laterYears = (humanAge - earlyYears) * 4;
+        myAgeInDogYears += laterYears;
+    } else if (humanAge < 2) {
+        myAgeInDogYears = humanAge * 10.5;
+    }
+    result.innerHTML = "Your age in dog years is " + myAgeInDogYears;
+};
+
+botao.addEventListener('click', convertToDogYears);
+age.addEventListener('keydown', function(e) {
+    if(e.key === 'Enter') {
+        e.preventDefault();
+        convertToDogYears();
+    }
+})
